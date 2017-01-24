676f6cc8ea58f3dacd4d938c23b24b671b365346
ClarkeW5 <clarkew5@mymacewan.ca>
2017-01-24 13:05:03 -0700

    fixes bug in making opening moves, and makes aesthetic changes to print board

--- game.c
+++ game.c
@@ -16,47 +16,47 @@ struct Board *makeBoard(char *filepath){
 }
 
 int printBoard(struct Board *b){
-    printf("+--------+\n");
+    printf(" +----------------+\n");
     for (int shift = 0; shift < 64; shift++){
         uint64_t i = 1L <<shift;
         if (shift%8 == 0)
-            printf("|");
+            printf("%d| ", (8 - (shift/8)));
 
         if ((i & b->gold[RABBIT]) == i)
-            printf("R");
+            printf("R ");
         else if ((i & b->silver[RABBIT]) == i)
-            printf("r");
+            printf("r ");
         else if ((i & b->gold[CAT]) == i)
-            printf("C");
+            printf("C ");
         else if ((i & b->silver[CAT]) == i)
-            printf("c");
+            printf("c ");
         else if ((i & b->gold[DOG]) == i)
-            printf("D");
+            printf("D ");
         else if ((i & b->silver[DOG]) == i)
-            printf("d");
+            printf("d ");
         else if ((i & b->gold[HORSE]) == i)
-            printf("H");
+            printf("H ");
         else if ((i & b->silver[HORSE]) == i)
-            printf("h");
+            printf("h ");
         else if ((i & b->gold[CAMMEL]) == i)
-            printf("M");
+            printf("M ");
         else if ((i & b->silver[CAMMEL]) == i)
-            printf("m");
+            printf("m ");
         else if ((i & b->gold[ELEPHANT]) == i)
-            printf("E");
+            printf("E ");
         else if ((i & b->silver[ELEPHANT]) == i)
-            printf("e");
+            printf("e ");
         else{
             if (shift == 18 || shift == 21 || shift == 42 || shift == 45)
-                printf("X");
+                printf("X ");
             else
-                printf("-");
+                printf("- ");
         }
 
         if (shift%8 == 7)
             printf("|\n");
     }
-    printf("+--------+\n");
+    printf(" +----------------+\n   a b c d e f g h\n");
 	return 0;
 }
 
@@ -124,6 +124,7 @@ int makeMove(char *move, struct Board *b){
             move += 4;
         }
         
-    }	
+    }
+	
     return 0;
 }
