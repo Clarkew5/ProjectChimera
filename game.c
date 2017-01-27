b39c17359ecb21f29a3c4cef8b2c6ee347c58b5a
ClarkeW5 <clarkew5@mymacewan.ca>
2017-01-26 17:52:37 -0700

    fixes some bugs and implements non opening moves

--- game.c
+++ game.c
@@ -56,7 +56,7 @@ int printBoard(struct Board *b){
         if (shift%8 == 7)
             printf("|\n");
     }
-    printf(" +----------------+\n   a b c d e f g h\n");
+    printf(" +-----------------+\n   a b c d e f g h\n");
 	return 0;
 }
 
@@ -65,9 +65,9 @@ int updateBoard(char *move, struct Board *b){
     char *rowNumbers = "87654321";
     if (strlen(move) == 63){
         printf("%s\n", move);
-        while (*move != '\0'){
+        do{
             int i = strchr(colLetters, *(move+1)) - colLetters;
-            int j = strchr(rowNumbers, *(move+2)) - rowNumbers;            
+            int j = strchr(rowNumbers, *(move+2)) - rowNumbers;        
             switch (*move){
                 case 'R':
                     b->gold[RABBIT] ^= 1L << ((8*j) + i);
@@ -119,12 +119,125 @@ int updateBoard(char *move, struct Board *b){
                     break;
                 default:
                     printf("error no such peice\n");
-                    return 0;
+                    return 1;
             }
             move += 4;
-        }
-        
+        } while (*(move-1) != '\0');
+                
     }
-	
+	else if ((strlen(move) < 21) && ((strlen(move) + 1)%5 == 0)){
+        printf("%s\n", move);        
+        do {
+            int i1 = strchr(colLetters, *(move+1)) - colLetters;
+            int j1 = strchr(rowNumbers, *(move+2)) - rowNumbers;
+            int i2 = 0;
+            int j2 = 0;
+            switch (*(move+3)){
+                case 'n':
+                    i2 = i1;
+                    j2 = j1-1;
+                    break;
+                case 'e':
+                    i2 = i1+1;
+                    j2 = j1;
+                    break;
+                case 's':
+                    i2 = i1;
+                    j2 = j1+1;
+                    break;
+                case 'w':
+                    i2 = i1;
+                    j2 = j2-1;
+                    break;
+                default:
+                    printf("error no such peice\n");
+                    return 1;
+            }        
+            switch (*move){
+                case 'R':
+                    b->gold[RABBIT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[RABBIT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);                    
+                    break;
+                case 'r':
+                    b->silver[RABBIT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[RABBIT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'C':
+                    b->gold[CAT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[CAT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);                    
+                    break;
+                case 'c':
+                    b->silver[CAT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[CAT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'D':
+                    b->gold[DOG] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[DOG] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'd':
+                    b->silver[DOG] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[DOG] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'H':
+                    b->gold[HORSE] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[HORSE] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'h':
+                    b->silver[HORSE] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[HORSE] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'M':
+                    b->gold[CAMMEL] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[CAMMEL] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'm':
+                    b->silver[CAMMEL] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[CAMMEL] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'E':
+                    b->gold[ELEPHANT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->gold[ELEPHANT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'e':
+                    b->silver[ELEPHANT] ^= 1L << ((8*j1) + i1);
+                    b->empty ^= 1L << ((8*j1) + i1);
+                    b->silver[ELEPHANT] ^= 1L << ((8*j2) + i2);
+                    b->empty ^= 1L << ((8*j2) + i2);
+                    break;
+                case 'p':
+                    return 0;
+                default:
+                    printf("error no such peice\n");
+                    return 1;
+            }
+            move += 5;
+        } while (*(move-1) != '\0');
+    }
+    else{
+        printf("Incorrect Input\n");
+        return 1;
+    }
     return 0;
 }
